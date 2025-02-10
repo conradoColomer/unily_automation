ef96e2ebf7e9f19776007b83b4c383a5c87751e3
conrado_unily_challenge <cmendezcolomer@gmail.com>
2025-02-10 20:05:45 -0300

    locator for products found. Continue with Eclipse locators strategy

--- lists/LIST_PRACTICE_IN_PYTHON/exercise_1.py
+++ lists/LIST_PRACTICE_IN_PYTHON/exercise_1.py
@@ -5,3 +5,20 @@
 # 2. Encuentra todos los elementos de una lista de productos usando find_elements().
 # 3. Imprime la cantidad total de elementos encontrados.
 # 4. Cierra el navegador.
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Inicializa el driver y la clase
+driver = webdriver.Chrome()
+driver.get("https://automationexercise.com")
+
+LIST_XPATH = (By.XPATH, "//div[contains(@class, 'productinfo text-center')]//p")
+
+productos =  WebDriverWait(driver,10).until(EC.presence_of_all_elements_located(LIST_XPATH))
+
+print(f"La cantidad de elementos ubicados es de: {len(productos)}")
+# Cierra el navegador al final
+driver.quit()
